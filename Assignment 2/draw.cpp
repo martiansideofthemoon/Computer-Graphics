440a2608db19593c5b2c4390bf74bb89ec999d00
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-09-28 23:48:20 +0530

    Finalizing directions

--- Assignment 2/draw.cpp
+++ Assignment 2/draw.cpp
@@ -15,45 +15,47 @@ int curr=0; //Index of current oprational node
 //Our function for processing ASCII keys
 void processNormalKeys(unsigned char key, int x, int y) {
   switch(key) {
-    case '1':
-      curr=0;
+    case 'a':
+    case 'A':
+      cycle->rotate(0, 1, 0);
       break;
-    case '2':
-      curr=1;
+    case 'd':
+    case 'D':
+      cycle->rotate(0, -1, 0);
       break;
-    case '3':
-      curr=2;
+    case 's':
+    case 'S':
+      cycle->rotate(1, 0, 0);
+      break;
+    case 'w':
+    case 'W':
+      cycle->rotate(-1, 0, 0);
       break;
   }
   if (key == 27)
   exit(0);
+  glutPostRedisplay();
 }
 
 //Our function for processing Non-ASCII keys
 void processSpecialKeys(int key, int x, int y) {
   switch(key) {
-    case GLUT_KEY_LEFT :
-    cycle->rotate(0,-1,0);
-    break;
-    case GLUT_KEY_RIGHT :
-    cycle->rotate(0,1,0);
-    break;
-    case GLUT_KEY_UP :
-    cycle->rotate(-1,0,0);
-    break;
-    case GLUT_KEY_DOWN :
-    cycle->rotate(1,0,0);
-    break;
     case GLUT_KEY_PAGE_UP :
-    cycle->pedal_cycle(-1);
+    cycle->rotate(0,0,1);
     break;
     case GLUT_KEY_PAGE_DOWN :
+    cycle->rotate(0,0,-1);
+    break;
+    case GLUT_KEY_UP :
     cycle->pedal_cycle(1);
     break;
-    case GLUT_KEY_HOME:
+    case GLUT_KEY_DOWN :
+    cycle->pedal_cycle(-1);
+    break;
+    case GLUT_KEY_LEFT:
     cycle->turn(1);
     break;
-    case GLUT_KEY_END:
+    case GLUT_KEY_RIGHT:
     cycle->turn(-1);
     break;
   }
@@ -79,7 +81,7 @@ void init(void)
   gluPerspective(100.0, 1.0, 1.0, 20.0);
 
   glMatrixMode(GL_MODELVIEW);
-  gluLookAt(0.0, 0.0, 3.0,  // eye is at (0,0,5)
+  gluLookAt(0.0, 0.0, 2.5,  // eye is at (0,0,5)
   0.0, 0.0, 0.0,      // center is at (0,0,0)
   0.0, 1.0, 0.0);      // up is in positive Y direction
 }
@@ -87,6 +89,7 @@ void init(void)
 int main(int argc, char **argv)
 {
   cycle = new Cycle("cycle.txt");
+  cycle->turn(30);
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
   glutInitWindowSize(640,640);
